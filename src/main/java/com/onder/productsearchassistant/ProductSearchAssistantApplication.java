181f4879a464510109474e0b71d60d647c6417c5
guzelogluonder <guzelogluonder@gmail.com>
2026-06-20 11:31:44 +0300

    Search Assistant App

---
+++ src/main/java/com/onder/productsearchassistant/ProductSearchAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.onder.productsearchassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSearchAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSearchAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onder/productsearchassistant/ProductSearchAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onder.productsearchassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSearchAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
